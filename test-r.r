18f753abf140842ff6359ccecb4e726237b7d7f7
Yamna Yasir <yamnaasif@gmail.com>
2022-03-10 13:02:52 -0800

    few lines added

--- test-r.r
+++ test-r.r
@@ -1,3 +1,8 @@
-# This is on r file
+# This is from Github
+print('Hello from Github')
 
-print('HELLO! R')
+Name = "Yamna"
+Version = "R Studio - In writing"
+
+print(Name)
+print("These lines have been added with R studio")
